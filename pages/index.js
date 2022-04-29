4515cf818932bf92238922037dd0957c2bd1d5c0
Intel Tech <jsreputation@gmail.com>
2022-04-29 23:31:56 +0800

    feat: get balance, get transaction history

--- pages/index.js
+++ pages/index.js
@@ -9,37 +9,62 @@ export default function Home() {
   const [provider, setProvider] = useState(null);
   const [signer, setSigner] = useState(null);
 
+  const [network, setNetwork] = useState(0);
+  const [address, setAddress] = useState(null);
   // initialize Web3Modal Settings on page load
-  useEffect(() => {
-    const _web3Modal = new Web3Modal({
-      network: "mainnet",
-      cacheProvider: true,
-      providerOptions: providerOptions,
-    });
-    setWeb3Modal(_web3Modal);
-  }, []);
+  // useEffect(() => {
+  //   const _web3Modal = new Web3Modal({
+  //     network: "mainnet",
+  //     cacheProvider: true,
+  //     providerOptions: providerOptions,
+  //   });
+  //   setWeb3Modal(_web3Modal);
+  // }, []);
 
   // Connect Wallet
-  const connectWeb3Modal = async () => {
-    try {
-      const instance = await web3Modal.connect();
-      if (instance) {
-        const _provider = new ethers.providers.Web3Provider(instance);
-        if (_provider) {
-          setProvider(_provider);
-          const { chainId } = await _provider.getNetwork();
-          console.log(chainId);
-          const _signer = _provider.getSigner();
-          if (_signer) {
-            setSigner(_signer);
-          } else {
-            console.log("No Signer Error");
-          }
-        }
-      }
-    } catch (error) {
-      console.log("Connection Error: ", error);
+  // const connectWeb3Modal = async () => {
+  //   try {
+  //     const instance = await web3Modal.connect();
+  //     if (instance) {
+  //       const _provider = new ethers.providers.Web3Provider(instance);
+  //       if (_provider) {
+  //         setProvider(_provider);
+  //         const { chainId } = await _provider.getNetwork();
+  //         console.log(chainId);
+  //         const _signer = _provider.getSigner();
+  //         if (_signer) {
+  //           setSigner(_signer);
+  //         } else {
+  //           console.log("No Signer Error");
+  //         }
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.log("Connection Error: ", error);
+  //   }
+  // };
+
+  useEffect(() => {
+    if (network && address) {
+      console.log("dddddddd");
+      connectProvider();
     }
+  }, [network, address]);
+
+  const connectProvider = async () => {
+    try {
+      const _provider = new ethers.providers.EtherscanProvider(
+        network,
+        "FM89RHIEKVUX6YP587FWWDG52J1XZGS7NC"
+      );
+      if (_provider) {
+        const history = await _provider.getHistory(address);
+        const balance = await _provider.getBalance(address);
+
+        console.log(history);
+        console.log(ethers.utils.formatEther(balance));
+      }
+    } catch (error) {}
   };
 
   const getBalance = async () => {
@@ -65,43 +90,56 @@ export default function Home() {
     }
   };
 
+  const changeNetwork = (network) => {
+    console.log(network.target.value);
+    setNetwork(network.target.value);
+  };
+
+  const changeAddress = (address) => {
+    console.log(address.target.value);
+    setAddress(address.target.value);
+  };
+
   return (
     <div>
       <Head>
         <title>Vezgo</title>
       </Head>
       <div className="container mx-auto">
-        <header>
+        <header className="mt-5">
           <label
             htmlFor="price"
             className="block text-sm font-medium text-gray-700"
           >
-            Price
+            Wallet Address
           </label>
           <div className="mt-1 relative rounded-md shadow-sm">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <span className="text-gray-500 sm:text-sm"> $ </span>
+              <span className="text-gray-500 sm:text-sm"> Address </span>
             </div>
             <input
               type="text"
               name="price"
               id="price"
-              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-              placeholder="0.00"
+              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-16 pr-15 sm:text-sm border-gray-300 rounded-md"
+              placeholder="0x..."
+              onChange={changeAddress}
             />
             <div className="absolute inset-y-0 right-0 flex items-center">
               <label htmlFor="currency" className="sr-only">
-                Currency
+                Network
               </label>
               <select
                 id="network"
                 name="currency"
-                className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+                className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-amber-300 bg-transparent text-gray-500 sm:text-sm rounded-md"
+                onChange={changeNetwork}
               >
-                <option>Ethereum Mainnet</option>
-                <option>Polygon</option>
-                <option>Fantom Opera</option>
-                <option>Binace Smart Chain</option>
+                <option value="homestead">Ethereum Mainnet</option>
+                <option value="ropsten">Ropsten</option>
+                <option value="rinkeby">Kovan</option>
+                <option value="goerli">Goerli</option>
+                <option value="rinkeby">Rinkeby</option>
               </select>
             </div>
           </div>
